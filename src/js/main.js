462c859186264095622d16c8c1acd3293831dee3
higorgatti <37638638+higorgatti@users.noreply.github.com>
2025-09-27 23:50:32 -0300

    Delete src/js/main.js

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import './ui.js';
-import './hackers.js';
-import { startGameLoop } from './game.js';
-
-document.getElementById('btnStart').addEventListener('click', () => {
-  document.getElementById('start').classList.remove('active');
-  document.getElementById('hud').classList.add('active');
-  startGameLoop();
-});
